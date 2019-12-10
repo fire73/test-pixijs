2d85c37d198b819fce9a944b1b0df40be7522653
Anton Kh <fire73.ru@yandex.ru>
2019-12-10 18:22:11 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      sizeMap: 15,
+      MAP: []
+    }
+
+    this.setState({
+      sizeMap: 25
+    })
+  }
+
+  // clickMapElement = () => {
+  //   console.log(`sd`)
+  // }
+
+  // generateMap = () => {
+  //   const map: number[][] = [];
+  //   for (let i = 0; i < this.sizeMap; i += 1) {
+  //     const mapCols: number[] = [];
+  //     for (let k = 0; k < this.sizeMap; k += 1) {
+  //       mapCols.push(i+k === 5 ? -1 : 0);
+  //     }
+  //     map.push(mapCols)
+  //   }
+  //   console.log(map);
+  //   return map;
+  // }
+
+  // // this.state = 
+
+  // getMap = (map: number[][], clickMapElement: any) => {
+  //   const mapRows: any[] = [];
+  //   map.forEach(row => {
+  //     const mapCols: any[] = [];
+  //     row.forEach(col => {
+  //       let className = 'mapElement';
+  //       if (col === -1) {
+  //         className = 'mapElementBlock'
+  //       }
+  //       mapCols.push(
+  //         <div className={className} onClick={clickMapElement}></div>
+  //       )
+  //     })
+  //     mapRows.push(
+  //       <div className="mapRow">
+  //         {mapCols}
+  //       </div>
+  //     )
+  //   })
+  //   return mapRows;
+  // }
+
+  render() {
+    return (
+      <div>
+        {this.state.sizeMap}
+        <div className="map">
+          {/* {this.getMap()} */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
