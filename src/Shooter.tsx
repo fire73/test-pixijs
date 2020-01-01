8895ca3f9709ab363f0130bdc9278e382cf1747e
AntonKh <fire73.ru@yandex.ru>
2020-01-02 02:43:38 +0300

    shooter: score | helth | semantic-ui | game over

--- src/Shooter.tsx
+++ src/Shooter.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './Shooter.css';
 import * as PIXI from 'pixi.js';
 import { Viewport } from 'pixi-viewport';
+import { Button, Header, Modal, Container } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
 
 interface CustomSprite extends PIXI.Sprite {
   vx: number;
@@ -24,6 +26,16 @@ type Point = {
 }
 
 export class Shooter extends React.Component {
+  state = {
+    score: 0,
+    helth: 50,
+    isOpenModal: false
+  };
+
+  ticker = new PIXI.Ticker();
+  player = PIXI.Sprite.from(`coloredspheres/sphere-01.png`) as CustomSprite;
+  bullets: CustomSprite[] = [];
+  enemies: CustomSprite[] = [];
 
   drawLinesBG(canvasW: number, canvasH: number, stage: PIXI.Container) {
     let linesY = 0;
@@ -57,8 +69,15 @@ export class Shooter extends React.Component {
   }
 
   componentDidMount() {
-    const viewportW = 2000;
-    const viewportH = 2000;
+    const isCollide = (obj1: CustomSprite, obj2: CustomSprite): Boolean => {
+      return (obj1.y + obj1.height / 2 >= obj2.y - obj2.height / 2
+        && obj1.y - obj1.height / 2 <= obj2.y + obj2.height / 2
+        && obj1.x + obj1.width / 2 >= obj2.x - obj2.width / 2
+        && obj1.x - obj1.width / 2 <= obj2.x + obj2.width / 2);
+    }
+
+    const viewportW = 1500;
+    const viewportH = 1500;
     const canvasW = window.innerWidth - 10;
     const canvasH = window.innerHeight - 10;
     // console.log(viewportW, viewportH);
@@ -89,8 +108,7 @@ export class Shooter extends React.Component {
 
     // const stage = new PIXI.Container();
     this.drawLinesBG(viewportW, viewportH, stage);
-
-    const player = PIXI.Sprite.from(`coloredspheres/sphere-01.png`) as CustomSprite;
+    const player = this.player;
     player.vx = 0;
     player.vy = 0;
     player.speed = 1.5;
@@ -103,14 +121,11 @@ export class Shooter extends React.Component {
       radius: 120
     });
 
-    let bullets: CustomSprite[] = [];
-
-    // const countEnemies = 3;
-    let enemies: CustomSprite[] = [];
-    // for (let i = 0; i < countEnemies; i += 1) {
-    //   createEnemy()
-    // }
-    function createEnemy() {
+    const countEnemies = 3;
+    for (let i = 0; i < countEnemies; i += 1) {
+      createEnemy(this.enemies);
+    }
+    function createEnemy(enemies: CustomSprite[]) {
       const enemy = PIXI.Sprite.from(`coloredspheres/sphere-11.png`) as CustomSprite;
       enemy.anchor.set(0.5, 0.5);
       enemy.width = 50;
@@ -132,11 +147,13 @@ export class Shooter extends React.Component {
         enemy.y = viewportH;
         enemy.x = _.random(viewportW);
       }
-      enemy.speed = 0.5 + _.random(1);
+      enemy.speed = 1 + _.random(2);
       let angle = Math.atan2(enemy.y - player.y, enemy.x - player.x) / Math.PI * 180;
       angle += 90;
       enemy.vx = Math.sin(angle * (Math.PI / - 180)) * enemy.speed;
       enemy.vy = Math.cos(angle * (Math.PI / - 180)) * enemy.speed;
+      enemy.deltaX = 0.1;
+      enemy.deltaY = 0.1;
       // enemy.deltaX = 0.02;
       // enemy.deltaY = 0.02;
       enemies.push(enemy);
@@ -144,18 +161,25 @@ export class Shooter extends React.Component {
     }
 
     // createEnemy();
-    // setInterval(() => {
-    //   createEnemy();
-    // }, 5000);
+    setInterval(() => {
+      createEnemy(this.enemies);
+    }, 5000);
 
     setInterval(() => {
-      enemies.forEach((enemy) => {
+      this.enemies.forEach((enemy) => {
         let angle = Math.atan2(enemy.y - player.y, enemy.x - player.x) / Math.PI * 180;
         angle += 90;
         enemy.vx = Math.sin(angle * (Math.PI / - 180)) * enemy.speed;
         enemy.vy = Math.cos(angle * (Math.PI / - 180)) * enemy.speed;
       });
     }, 2000);
+
+    const gameOver = () => {
+      this.ticker.stop();
+      this.setState({
+        isOpenModal: true
+      });
+    }
 
     renderer.plugins.interaction.on('pointerup', (event: any) => {
       // console.log(event.data.global.x.toFixed(2), event.data.global.y.toFixed(2), {
@@ -175,19 +199,20 @@ export class Shooter extends React.Component {
       bullet.height = 10;
       bullet.deltaX = 0.5;
       bullet.deltaY = 0.5;
+      bullet.name = 'bullet';
       bullet.pointTo = {
         x: clX,
         y: clY
       }
       let angle = Math.atan2(bullet.y - bullet.pointTo.y, bullet.x - bullet.pointTo.x) / Math.PI * 180;
       angle += 90;
-      bullet.speed = 4;
+      bullet.speed = 7;
 
       bullet.vx = Math.sin(angle * (Math.PI / - 180)) * bullet.speed;
       bullet.vy = Math.cos(angle * (Math.PI / - 180)) * bullet.speed;
 
       stage.addChild(bullet);
-      bullets.push(bullet);
+      this.bullets.push(bullet);
     });
 
     window.addEventListener(
@@ -208,18 +233,25 @@ export class Shooter extends React.Component {
           player.vx = player.speed;
           event.preventDefault();
         }
-        // if (event.keyCode === 32) {
-        //   console.log('PIFF PAF',
-        //   {
-        //     x: player.x,
-        //     y: player.y,
-        //     width: player.width,
-        //     height: player.height
-        //   });
+        if (event.keyCode === 32) {
+          //   console.log('PIFF PAF',
+          //   {
+          //     x: player.x,
+          //     y: player.y,
+          //     width: player.width,
+          //     height: player.height
+          //   });
 
 
-        //   event.preventDefault();
-        // }
+          //   event.preventDefault();
+          console.log(this.bullets);
+          const bulletsBug = stage.children.filter(x => x.name === 'bullet');
+          console.log('bull length', bulletsBug.length);
+          bulletsBug.forEach(b => {
+            stage.removeChild(b);
+          })
+
+        }
       }, false
     );
     window.addEventListener(
@@ -253,8 +285,8 @@ export class Shooter extends React.Component {
 
     stage.addChild(player);
 
-    const ticker = new PIXI.Ticker();
-    ticker.add(() => {
+    // const ticker = new PIXI.Ticker();
+    this.ticker.add(() => {
       renderer.render(stage);
       player.rotation += 0.05;
       if (player.y - player.height / 2 >= 0 && player.y + player.height / 2 <= viewportH
@@ -276,62 +308,88 @@ export class Shooter extends React.Component {
       }
       // delta += 0.05;
       // player.scale.set(0.2 + Math.sin(delta) / 10)
-      bullets.forEach((bull, key) => {
+      this.bullets.forEach((bull, key) => {
         // bull.y += 3;
-        // bull.deltaX += 0.2;
-        // bull.deltaY += 0.2;
-        // bull.x += Math.sin(bull.deltaX) * 5;
-        // bull.y += Math.cos(bull.deltaY) * 5;
+        bull.deltaX += 0.2;
+        bull.deltaY += 0.2;
+        bull.x += Math.sin(bull.deltaX) * 5;
+        bull.y += Math.cos(bull.deltaY) * 5;
 
         bull.x += bull.vx;
         bull.y += bull.vy;
         if (bull.y > viewportH || bull.y < 0 || bull.x < 0 || bull.x > viewportW) {
           stage.removeChild(bull);
-          bullets.splice(key, 1);
+          this.bullets.splice(key, 1);
         }
-
-        enemies.forEach((enemy, keyEnemy) => {
-          if (
-            bull.y + bull.height / 2 >= enemy.y - enemy.height / 2
-            && bull.y - bull.height / 2 <= enemy.y + enemy.height / 2
-            && bull.x + bull.width / 2 >= enemy.x - enemy.width / 2
-            && bull.x - bull.width / 2 <= enemy.x + enemy.width / 2
-          ) {
-            // console.log('COLLIDE');
-            stage.removeChild(bull);
-            bullets.splice(key, 1);
-            stage.removeChild(enemy);
-            enemies.splice(keyEnemy, 1);
-          }
-        });
-
       });
 
-      // enemies.forEach((enemy, key) => {
-      //   // enemy
-      //   enemy.deltaX += 0.05;
-      //   enemy.deltaY += 0.05;
-      //   enemy.x += Math.sin(enemy.deltaX) * 2;
-      //   enemy.y += Math.cos(enemy.deltaY) * 2;
-      //   enemy.x += enemy.speed;
-      //   enemy.y += enemy.speed;
-
-      //   if (enemy.x > canvasH) {
-      //     stage.removeChild(enemy);
-      //     enemies.splice(key, 1);
-      //   }
-      // });
-      enemies.forEach((enemy, key) => {
+      this.enemies.forEach((enemy, key) => {
+        enemy.deltaX += 0.1;
+        enemy.deltaY += 0.1;
+        enemy.x += Math.sin(enemy.deltaX) * 2;
+        enemy.y += Math.cos(enemy.deltaY) * 2;
         enemy.x += enemy.vx;
         enemy.y += enemy.vy;
+        if (isCollide(enemy, player)) {
+          this.setState({
+            helth: this.state.helth - 0.3
+          });
+          if (this.state.helth <= 0) {
+            // end game
+            gameOver();
+          }
+        }
+        this.bullets.forEach((bull, keyBull) => {
+          if (isCollide(enemy, bull)) {
+            stage.removeChild(bull);
+            this.bullets.splice(keyBull, 1);
+            stage.removeChild(enemy);
+            this.enemies.splice(key, 1);
+            // this.state.score 
+            this.setState({
+              score: this.state.score + 1
+            });
+          }
+        });
       });
     });
-    ticker.start();
+    this.ticker.start();
+  }
+
+  componentWillUnmount() {
+    this.ticker.stop();
+  }
+
+  restartGame = () => {
+    // this.setState({
+    //   isOpenModal: false,
+    //   score: 0,
+    //   helth: 100
+    // });
+    document.location.reload();
+    // this.ticker.start();
   }
 
   render() {
     return <div className="Shooter">
+      <div className="scoreBox">
+        <div>score: {this.state.score}</div>
+        <div>helth: {Math.ceil(this.state.helth)}</div>
+      </div>
+
       <canvas id="canvasGame"></canvas>
+      
+      <Modal basic size='small' open={this.state.isOpenModal}>
+        <Header icon='browser' content='Game over' />
+        <Modal.Content>
+          <h3>your score: {this.state.score}</h3>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color='green' onClick={this.restartGame} inverted>
+            ok
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   }
 }
